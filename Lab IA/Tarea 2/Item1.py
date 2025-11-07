09974b89bb923e22fabef9b44ec095fd6b2744d0
Branxeto <brancoburotto@gmail.com>
2025-11-07 19:31:44 -0300

    ...

--- Lab IA/Tarea 2/Item1.py
+++ Lab IA/Tarea 2/Item1.py
@@ -1,1 +1,12 @@
-#Este es el item 1
+#Deberá ejecutar tres algoritmos de clustering: K-Means, K-Means++ y
+#MeanShift, utilizando al menos cuatro configuraciones distintas para cada técnica
+#(deberá justificar la elección de parámetros). Para el entrenamiento, use únicamente
+#el 80% de los datos, omitiendo la etiqueta Y. A continuación, evalúe las doce
+#configuraciones obtenidas mediante una métrica de su elección (por ejemplo,
+#Silhouette Score) y seleccione las tres de mejor desempeño. Luego, aplique cada
+#una de estas configuraciones al 20% restante de los datos, asignando a cada muestra
+#el cluster correspondiente (obtenidos desde el entrenamiento). Finalmente, compare
+#la etiqueta Y real de cada muestra con la etiqueta dominante dentro del cluster al
+#que pertenece y analice si este procedimiento resulta razonable para asignar
+#etiquetas faltantes.
+
--- Lab IA/Tarea 2/Item2.py
+++ Lab IA/Tarea 2/Item2.py
@@ -1,1 +1,13 @@
-#Este es el item 2
+#Utilizando el mismo conjunto de datos previamente seleccionado,
+#diseñe e implemente una técnica que permita entrenar en paralelo múltiples
+#instancias (al menos tres por técnica) de Regresión Logística y SVM, variando sus
+#hiperparámetros (por ejemplo: batch size, tasa de aprendizaje, etc, según sea el
+#caso). Los parámetros de cada configuración deberán definirse en un archivo de
+#configuración externo, y el entrenamiento deberá realizarse utilizando el 80% de los
+#datos. Cada modelo se evaluará periódicamente (solo con datos de entrenamiento),
+#y por cada cinco épocas deberá descartarse la configuración con peor desempeño
+#entre todas las configuraciones restantes. Para las dos mejores configuraciones,
+#presente métricas de evaluación utilizando el conjunto de testing, es decir, el 20%
+#de los datos. Analice los resultados obtenidos en función de los hiperparámetros
+#seleccionados.
+
